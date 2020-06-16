e5ed4b1b040510fb33e2f26bc0bdf2e3bb58da7d
Julien <julien.rondin@epitech.eu>
2020-06-16 14:21:45 +0200

    [DONE] Corrected everything, added lists of languages

--- interface.py
+++ interface.py
@@ -1,68 +1,171 @@
-from tkinter import * 
+from tkinter import *
 from googletrans import Translator
 
 global sentence
 global selected_language
 global result_translate
 
+LANGUAGES = {
+    'af': 'afrikaans',
+    'sq': 'albanian',
+    'am': 'amharic',
+    'ar': 'arabic',
+    'hy': 'armenian',
+    'az': 'azerbaijani',
+    'eu': 'basque',
+    'be': 'belarusian',
+    'bn': 'bengali',
+    'bs': 'bosnian',
+    'bg': 'bulgarian',
+    'ca': 'catalan',
+    'ceb': 'cebuano',
+    'ny': 'chichewa',
+    'zh-cn': 'chinese (simplified)',
+    'zh-tw': 'chinese (traditional)',
+    'co': 'corsican',
+    'hr': 'croatian',
+    'cs': 'czech',
+    'da': 'danish',
+    'nl': 'dutch',
+    'en': 'english',
+    'eo': 'esperanto',
+    'et': 'estonian',
+    'tl': 'filipino',
+    'fi': 'finnish',
+    'fr': 'french',
+    'fy': 'frisian',
+    'gl': 'galician',
+    'ka': 'georgian',
+    'de': 'german',
+    'el': 'greek',
+    'gu': 'gujarati',
+    'ht': 'haitian creole',
+    'ha': 'hausa',
+    'haw': 'hawaiian',
+    'iw': 'hebrew',
+    'hi': 'hindi',
+    'hmn': 'hmong',
+    'hu': 'hungarian',
+    'is': 'icelandic',
+    'ig': 'igbo',
+    'id': 'indonesian',
+    'ga': 'irish',
+    'it': 'italian',
+    'ja': 'japanese',
+    'jw': 'javanese',
+    'kn': 'kannada',
+    'kk': 'kazakh',
+    'km': 'khmer',
+    'ko': 'korean',
+    'ku': 'kurdish (kurmanji)',
+    'ky': 'kyrgyz',
+    'lo': 'lao',
+    'la': 'latin',
+    'lv': 'latvian',
+    'lt': 'lithuanian',
+    'lb': 'luxembourgish',
+    'mk': 'macedonian',
+    'mg': 'malagasy',
+    'ms': 'malay',
+    'ml': 'malayalam',
+    'mt': 'maltese',
+    'mi': 'maori',
+    'mr': 'marathi',
+    'mn': 'mongolian',
+    'my': 'myanmar (burmese)',
+    'ne': 'nepali',
+    'no': 'norwegian',
+    'ps': 'pashto',
+    'fa': 'persian',
+    'pl': 'polish',
+    'pt': 'portuguese',
+    'pa': 'punjabi',
+    'ro': 'romanian',
+    'ru': 'russian',
+    'sm': 'samoan',
+    'gd': 'scots gaelic',
+    'sr': 'serbian',
+    'st': 'sesotho',
+    'sn': 'shona',
+    'sd': 'sindhi',
+    'si': 'sinhala',
+    'sk': 'slovak',
+    'sl': 'slovenian',
+    'so': 'somali',
+    'es': 'spanish',
+    'su': 'sundanese',
+    'sw': 'swahili',
+    'sv': 'swedish',
+    'tg': 'tajik',
+    'ta': 'tamil',
+    'te': 'telugu',
+    'th': 'thai',
+    'tr': 'turkish',
+    'uk': 'ukrainian',
+    'ur': 'urdu',
+    'uz': 'uzbek',
+    'vi': 'vietnamese',
+    'cy': 'welsh',
+    'xh': 'xhosa',
+    'yi': 'yiddish',
+    'yo': 'yoruba',
+    'zu': 'zulu',
+    'fil': 'filipino',
+    'he': 'hebrew'
+}
 
-fenetre = Tk()
+window = Tk()
+window.title("My translator")
 
-fenetre.geometry("595x323")
+window.geometry("600x300")
 
-sentence_label = Label(fenetre, text="Enter your sentence : ")
+sentence_label = Label(window, text="Enter your sentence : ")
 
+sentence = StringVar()
+sentence.set("Enter a sentence")
+entree = Entry(window, textvariable=sentence, width=30)
 
-sentence = StringVar() 
-sentence.set("your sentence")
-entree = Entry(fenetre, textvariable=sentence, width=30)
+language_label = Label(window, text="Chose your language : ")
 
-
-language_label = Label(fenetre, text="Chose your language : ")
-
-
-language_liste = Listbox(fenetre)
-language_liste.insert(1, "fr")
-language_liste.insert(2, "en")
-language_liste.insert(3, "ru")
-language_liste.insert(4, "ja")
+language_liste = Listbox(window)
+i = 0
+for lab, lang in LANGUAGES.items():
+    language_liste.insert(i, lang)
+    i += 1
 
 selected_language = StringVar()
+
+translate_bouton=Button(window, text="Translate", command=lambda : translate_func())
+quit_button=Button(window, text="Quit", command=window.quit)
+translate_label = Label(window, text="Translation", bg="yellow")
 
 def updateLabel():
     line = language_liste.curselection()[0]
     item = language_liste.get(line)
     selected_language.set(item)
 
-translate_bouton=Button(fenetre, text="traduire", command=lambda : translate_func())
-
-
-quit_button=Button(fenetre, text="quitter", command=fenetre.quit)
-result_translate = Label(fenetre, text="your result",bg="yellow")
 
 def translate(sent, country):
     translator = Translator()
-    translate_result = translator.translate(sent, dest=country)
-    print(translate_result.text)
-    result_translate = Label(fenetre, text=translate_result.text, bg="blue")
-    fenetre.update
-    
-    
-def createall() : 
+    res = translator.translate(sent, dest=country)
+    translate_label['text'] = "{} -> {}".format(sent, res.text)
+    window.update()
+
+def createall() :
     sentence_label.pack()
     entree.pack()
     language_label.pack()
     language_liste.pack()
     translate_bouton.pack()
     quit_button.pack()
-    result_translate.pack()
-
-
+    translate_label.pack()
 
 def translate_func() :
     updateLabel()
-    translate(sentence.get(), selected_language.get())
-   
+    if (selected_language):
+        translate(sentence.get(), selected_language.get())
+    else:
+        print("Please select a language.")
 
 createall()
-fenetre.mainloop()
+window.mainloop()
--- translate.py
+++ translate.py
@@ -1,15 +1,137 @@
 from googletrans import Translator
 
-print('Enter your sentence:')
-x = input()
+LANGUAGES = {
+    'af': 'afrikaans',
+    'sq': 'albanian',
+    'am': 'amharic',
+    'ar': 'arabic',
+    'hy': 'armenian',
+    'az': 'azerbaijani',
+    'eu': 'basque',
+    'be': 'belarusian',
+    'bn': 'bengali',
+    'bs': 'bosnian',
+    'bg': 'bulgarian',
+    'ca': 'catalan',
+    'ceb': 'cebuano',
+    'ny': 'chichewa',
+    'zh-cn': 'chinese (simplified)',
+    'zh-tw': 'chinese (traditional)',
+    'co': 'corsican',
+    'hr': 'croatian',
+    'cs': 'czech',
+    'da': 'danish',
+    'nl': 'dutch',
+    'en': 'english',
+    'eo': 'esperanto',
+    'et': 'estonian',
+    'tl': 'filipino',
+    'fi': 'finnish',
+    'fr': 'french',
+    'fy': 'frisian',
+    'gl': 'galician',
+    'ka': 'georgian',
+    'de': 'german',
+    'el': 'greek',
+    'gu': 'gujarati',
+    'ht': 'haitian creole',
+    'ha': 'hausa',
+    'haw': 'hawaiian',
+    'iw': 'hebrew',
+    'hi': 'hindi',
+    'hmn': 'hmong',
+    'hu': 'hungarian',
+    'is': 'icelandic',
+    'ig': 'igbo',
+    'id': 'indonesian',
+    'ga': 'irish',
+    'it': 'italian',
+    'ja': 'japanese',
+    'jw': 'javanese',
+    'kn': 'kannada',
+    'kk': 'kazakh',
+    'km': 'khmer',
+    'ko': 'korean',
+    'ku': 'kurdish (kurmanji)',
+    'ky': 'kyrgyz',
+    'lo': 'lao',
+    'la': 'latin',
+    'lv': 'latvian',
+    'lt': 'lithuanian',
+    'lb': 'luxembourgish',
+    'mk': 'macedonian',
+    'mg': 'malagasy',
+    'ms': 'malay',
+    'ml': 'malayalam',
+    'mt': 'maltese',
+    'mi': 'maori',
+    'mr': 'marathi',
+    'mn': 'mongolian',
+    'my': 'myanmar (burmese)',
+    'ne': 'nepali',
+    'no': 'norwegian',
+    'ps': 'pashto',
+    'fa': 'persian',
+    'pl': 'polish',
+    'pt': 'portuguese',
+    'pa': 'punjabi',
+    'ro': 'romanian',
+    'ru': 'russian',
+    'sm': 'samoan',
+    'gd': 'scots gaelic',
+    'sr': 'serbian',
+    'st': 'sesotho',
+    'sn': 'shona',
+    'sd': 'sindhi',
+    'si': 'sinhala',
+    'sk': 'slovak',
+    'sl': 'slovenian',
+    'so': 'somali',
+    'es': 'spanish',
+    'su': 'sundanese',
+    'sw': 'swahili',
+    'sv': 'swedish',
+    'tg': 'tajik',
+    'ta': 'tamil',
+    'te': 'telugu',
+    'th': 'thai',
+    'tr': 'turkish',
+    'uk': 'ukrainian',
+    'ur': 'urdu',
+    'uz': 'uzbek',
+    'vi': 'vietnamese',
+    'cy': 'welsh',
+    'xh': 'xhosa',
+    'yi': 'yiddish',
+    'yo': 'yoruba',
+    'zu': 'zulu',
+    'fil': 'filipino',
+    'he': 'hebrew'
+}
 
-print('Enter the language you want exp (fr,en,ja,etc...)')
-
-y = input ()
-
-def translate(sent, country):
+def translate(input, country):
     translator = Translator()
-    translate_result = translator.translate(sent, dest=country)
-    print(translate_result.text)
+    translate_result = translator.translate(input, dest=country)
+    print(input + "  ->  " + translate_result.text)
+    print()
 
-translate(x, y)
+def check_lang(input_sentance):
+    for init, lang in LANGUAGES.items():
+        if (lang.find(input_sentance.lower()) or input_sentance == str(init)):
+            return True
+    return False
+
+def main():
+    while 1:
+        print("Enter a sentence to translate:")
+        input_sentance = input()
+        print("Enter a translate language:")
+        input_lang = input()
+        while (check_lang(input_lang) == False):
+            print("Error: Please enter a correct language")
+            input_lang = input()
+        translate(input_sentance, input_lang)
+    return 0
+
+if __name__ == "__main__":
+    main()
